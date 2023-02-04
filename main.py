900b402ee9e3109fb4eac91e67aa85474abcc15c
Luis-D <luisnestordl@hotmail.com>
2023-02-04 10:32:01 -0400

    Bases para Character, controles y batches de sprite

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-import pygame
-from pygame.locals import *
-from src.core.constants import *
-from src.core.functions import *
-
-from src.core.globalscope import * 
-
 
 import sys
+import pygame
+from pygame.locals import *
+
+from src.core.constants import *
+from src.core.functions import *
+from src.core.globalscope import * 
+from src.core.Events import *
+
 
 
 def main():
     pygame.init()
 #    pygame.mixer.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption(TITLE)
 
-    Global.Init(sys.argv[1]);
+    try:
+        Global.Init(sys.argv[1]);
+    except:
+        sys.exit()
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        E_process();
         Global.Update(Global);
         Global.Draw(Global);
         pygame.display.flip()
---
+++ src/core/spritesheets.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Spritebatch:
+    def __init__(self,FILE,r,g,b):
+        self.sheet = pygame.image.load(FILE).convert()
+        self.colorkey = (r,g,b)
+        self.hashtable = {}
+    def image_at(self,x,y,dx,dy):
+        hsh = x+y+dx+dy;
+        try:
+            img = self.hashtable[hsh];
+            return img
+        except KeyError:
+            print("Nuevo pedazo de batch")
+            rect = pygame.Rect((x,y,dx,dy))
+            image = pygame.Surface(rect.size).convert()
+            image.blit(self.sheet,(0,0),rect)
+            image.set_colorkey(self.colorkey,pygame.RLEACCEL)
+            self.hashtable[hsh] = image
+            return image
