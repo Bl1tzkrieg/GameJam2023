cc730f26c76caa45ee71957332622b7aaa59af91
Luis-D <luisnestordl@hotmail.com>
2023-02-03 21:32:14 -0400

    Propuesta de nivel dinámico

---
+++ src/assets/scripts/test.py
@@ -0,0 +1,9 @@
+from core.globalscope import *
+
+
+def lmao(self):
+    print("LMAO")
+    Global.Update = nivel2
+
+
+Global.Update = lmao
---
+++ src/core/globalscope.py
@@ -0,0 +1,9 @@
+class State:
+    def Update(self):
+        pass
+    def Draw(self):
+        pass
+    def Init(self):
+        print("start")
+
+Global = State();
