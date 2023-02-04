d0e7e557a0b573ff76736fefa736ac23ed7b9aab
Luis-D <luisnestordl@hotmail.com>
2023-02-04 11:08:16 -0400

    suprimido el colorkey

--- assets/scripts/testbatch.py
+++ assets/scripts/testbatch.py
@@ -9,7 +9,7 @@ from src.core.Char import *
 import sys
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-spri = Spritebatch("assets/sprites/fondo.jpg",255,255,255)
+spri = Spritebatch("assets/sprites/fondo.jpg",None)
 
 npc = Char(0,0,"Changuito")
 npc.set_images([spri.image_at(0,0,256,256),spri.image_at(64,0,256,256)])
--- src/core/spritesheets.py
+++ src/core/spritesheets.py
@@ -2,9 +2,9 @@ import pygame
 from src.core.functions import *
 
 class Spritebatch:
-    def __init__(self,FILE,r,g,b):
+    def __init__(self,FILE,colorkey):
         self.sheet = load_image(FILE,alpha=True)
-        self.colorkey = (r,g,b)
+        self.colorkey = colorkey
         self.hashtable = {}
     def image_at(self,x,y,dx,dy):
         hsh = x+y+dx+dy;
@@ -16,6 +16,7 @@ class Spritebatch:
             rect = pygame.Rect((x,y,dx,dy))
             image = pygame.Surface(rect.size).convert()
             image.blit(self.sheet,(0,0),rect)
-            image.set_colorkey(self.colorkey,pygame.RLEACCEL)
+            if(self.colorkey != None):
+                image.set_colorkey(self.colorkey,pygame.RLEACCEL)
             self.hashtable[hsh] = image
             return image
