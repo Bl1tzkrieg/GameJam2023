76d4b52a43a018243b5c7bb2186b6610bb2ffde0
Luis Bermudez <luis.bermudez.1996.ve@gmail.com>
2023-02-03 20:47:33 -0400

    added background

--- core/functions.py
+++ core/functions.py
@@ -4,16 +4,23 @@ import os
 import sys
 
 def load_image(nombre, dir_imagen, alpha=False):
-    # Encontramos la ruta completa de la imagen
     ruta = os.path.join(dir_imagen, nombre)
     try:
         image = pygame.image.load(ruta)
     except:
         print("Error, no se puede cargar la imagen: " + ruta)
         sys.exit(1)
-    # Comprobar si la imagen tiene "canal alpha" (como los png)
     if alpha is True:
         image = image.convert_alpha()
     else:
         image = image.convert()
     return image
+
+def load_sound(nombre, dir_sonido):
+    ruta = os.path.join(dir_sonido, nombre)
+    try:
+        sonido = pygame.mixer.Sound(ruta)
+    except (pygame.error) as message:
+        print("No se pudo cargar el sonido:" + ruta)
+        sonido = None
+    return sonido
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from pygame.locals import *
 from core.constants import *
+from core.functions import *
 
 import sys
 
 def main():
     pygame.init()
+    pygame.mixer.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption(TITLE)
-    
+    background = load_image("fondo.jpg", ASSETS_DIR+"sprites", alpha=False)
+
     while True:
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
