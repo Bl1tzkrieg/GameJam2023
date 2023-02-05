aa09eabecaaef74ceaeb24e4e2ede9e25baea0a8
Luis Bermudez <luis.bermudez.1996.ve@gmail.com>
2023-02-04 23:05:34 -0400

    agregado sonidos chango y fondo

--- assets/scripts/menu.py
+++ assets/scripts/menu.py
@@ -38,18 +38,30 @@ pygame.font.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 fondo = load_image(ASSETS_DIR+"sprites/fondo.jpg", True).convert()
 fondo = pygame.transform.scale(fondo, (SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.mixer.init()
+pygame.mixer.music.load(ASSETS_DIR+"sounds/menu.ogg")
 menu = Menu(opciones)
 
 def DrawBG(self):
-    screen.blit(fondo,(0,0))
-    menu.imprimir(screen)
+    if Global.width_t < SCREEN_WIDTH:
+        Global.width_t = Global.width_t+(Global.W/120)
+    if Global.height_t < SCREEN_HEIGHT:
+        Global.height_t = Global.height_t+(Global.H/120)
+    screen.blit(pygame.transform.scale(fondo, (Global.width_t, Global.height_t)),(0,0))
+    if Global.timer == 0:
+        menu.imprimir(screen)
 
 def Update(self):
     if(Controles.esc == True):
         sys.exit()
-    menu.actualizar()
-    
+    if Global.timer == 0:
+        menu.actualizar()
+    else:
+        Global.timer = Global.timer-1
 
 def Init():
+    pygame.mixer.music.play()
+    pygame.event.wait()
+    Global.timer = 120
     Global.Draw = DrawBG
     Global.Update = Update
